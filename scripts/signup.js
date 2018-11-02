297d22dae24799f0a045f402c9d8b6d92a752a01
Deacon Rodda <deacon@sqglz.com>
2018-11-02 17:42:22 -0600

    modal opens and closes in all predictable ways

--- scripts/signup.js
+++ scripts/signup.js
@@ -2,9 +2,16 @@
 
 var modalButton = document.getElementById("signup-button");
 var modal = document.getElementById("signup-modal");
+var backdrop = document.getElementById("modal-backdrop");
+var closeX = document.getElementById("close-x");
 
-function openModal() {
-	console.log("modal opening");
+function toggleModal() {
+
+	modal.classList.toggle("signup__hidden");
+	backdrop.classList.toggle("signup__hidden");
+	// console.log("modal opening");
 }
 
-modalButton.addEventListener("click", openModal);
+modalButton.addEventListener("click", toggleModal);
+backdrop.addEventListener("click", toggleModal);
+closeX.addEventListener("click", toggleModal);
